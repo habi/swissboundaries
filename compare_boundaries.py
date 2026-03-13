6bfa195ba8f8e9d950b487429275afe522233d62
David Haberthür <email@davidhaberthuer.ch>
2026-03-13 14:17:57 +0100

    Add reference link to repository

--- compare_boundaries.py
+++ compare_boundaries.py
@@ -1342,7 +1342,6 @@ def create_index_page():
         readme_json = json.dumps(readme_text)
         readme_section = f"""
     <section class=\"framed-section\">
-        <h3>README</h3>
         <div id=\"readme-content\"></div>
         <script>
             (function() {{
@@ -1417,15 +1416,13 @@ def create_index_page():
 </head>
 <body>
 
-
-
     <h2><a href="https://www.swisstopo.admin.ch/en/landscape-model-swissboundaries3d">swissBOUNDARIES3D</a> <-> <a href="https://overpass-turbo.eu/s/2jcH">OpenStreetMap</a></h2>
     
     <p>
         Comparison of Swiss municipality boundaries between official Swisstopo data (<a href="https://www.swisstopo.admin.ch/en/landscape-model-swissboundaries3d">swissBOUNDARIES3D</a>) and <a href="https://www.openstreetmap.org/#map=9/46.823/7.880">OpenStreetMap</a>.<br>
         The table below shows the latest comparison results for each municipality, including metrics like IoU, area difference, Hausdorff distance, and more.<br>
         You can search, sort, and download the data in various formats.<br>
-        For detailed explanations of the metrics and methodology, please refer to the README section below.<br>
+        For detailed explanations of the metrics and methodology, please refer to the README section below, which is directly pulled from <a href="https://github.com/habi/swissboundaries">the code repository</a>.<br>
         The bottom of the page shows a plot of the calculated metrics over time.
     </p>
 
